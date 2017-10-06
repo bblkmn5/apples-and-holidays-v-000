9ed34949c270a2279204bf538cae32025bedd367
Ben Blackman <bblkmn5@gmail.com>
2017-10-06 20:11:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].map do |holiday, supplies|
     supplies
   end
-  holiday_hash[:winter].flatten
+  flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
