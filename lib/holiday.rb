3a87d89430fee20e58eac2c53d6d26902326a92e
Ben Blackman <bblkmn5@gmail.com>
2017-10-06 20:37:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season}: "
     holidays.each do |holiday, supplies|
       puts "#{holiday.to_s.split("_").each {|v| v.join(" ")}}: #{supplies.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
